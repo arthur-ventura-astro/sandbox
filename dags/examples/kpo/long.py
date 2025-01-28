4ba27fdd01c0048cd3f33369a3c5852239a6683f
arthur-ventura-astro <arthurplautz.ventura@astronomer.io>
2025-01-28 12:27:34 -0300

    Fixing long running tasks example

--- dags/examples/kpo/long.py
+++ dags/examples/kpo/long.py
@@ -5,16 +5,6 @@ from datetime import datetime
 NAMESPACE = conf.get("kubernetes", "NAMESPACE")
 ITERATIONS = (2 * 24 * 60)
 
-def _long_loop(iter, sleep_time=60):
-    from time import sleep
-
-    print("Started")
-    for i in range(iter):
-        sleep(sleep_time)
-        print(f"Iteration [{i}/{iter}]")
-    print("Finished")
-
-
 @dag(
     schedule="@once",
     start_date=datetime(2024, 11, 29),
@@ -29,8 +19,14 @@ def kpo_long_running_example():
         get_logs=True,
         image_pull_policy="Always"
     )
-    def long_running(iter):
-        _long_loop(iter)
+    def long_running(iter, sleep_time=60):
+        from time import sleep
+
+        print("Started")
+        for i in range(iter):
+            sleep(sleep_time)
+            print(f"Iteration [{i}/{iter}]")
+        print("Finished")
 
     @task.kubernetes(
         image="python",
@@ -40,8 +36,14 @@ def kpo_long_running_example():
         image_pull_policy="Always",
         queue='isolated'
     )
-    def long_running_isolated(iter):
-        _long_loop(iter)
+    def long_running_isolated(iter, sleep_time=60):
+        from time import sleep
+
+        print("Started")
+        for i in range(iter):
+            sleep(sleep_time)
+            print(f"Iteration [{i}/{iter}]")
+        print("Finished")
 
     long_running(iter=ITERATIONS)
     long_running_isolated(iter=ITERATIONS)
