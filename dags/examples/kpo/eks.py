ce822056162cd7ccf9a7aa038dd50a41eed467bc
arthur-ventura-astro <arthurplautz.ventura@astronomer.io>
2025-01-31 00:27:16 -0300

    Add EksPodOperator example

---
+++ dags/examples/kpo/eks.py
@@ -0,0 +1,23 @@
+from airflow.decorators import dag
+from datetime import datetime
+from airflow.providers.amazon.aws.operators.eks import EksPodOperator
+
+@dag(
+    schedule="0 * * * *",
+    start_date=datetime(2024, 11, 29),
+    catchup=False,
+    tags=["examples"],
+)
+def eks_remote_example():
+    task = EksPodOperator(
+        task_id="test_pod",
+        pod_name="test_pod",
+        cluster_name="astro-sandbox-cluster",
+        namespace="default",
+        image="hello-world",
+        get_logs=True,
+        is_delete_operator_pod=True,
+        aws_conn_id='aws_default'
+    )
+
+eks_remote_example()
