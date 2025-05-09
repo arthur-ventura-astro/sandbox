d615d5ba07bcb0cccc588153f5d4b3ffa5c0cfc0
arthur-ventura-astro <arthurplautz.ventura@astronomer.io>
2025-05-09 12:57:36 -0300

    Fixing dbt project path

--- dags/traffic-stuff/transformation.py
+++ dags/traffic-stuff/transformation.py
@@ -31,7 +31,7 @@ def traffic_accidents_dbt() -> None:
     DbtTaskGroup(
         group_id="transform_task_group",
         project_config=ProjectConfig(
-            dbt_project_path=DBT_ROOT_PATH / "accident_accidents"
+            dbt_project_path=DBT_ROOT_PATH / "accident_factors"
         ),
         profile_config=profile_config
     )
